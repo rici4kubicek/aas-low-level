52fdb53ef346489924921e470194ef0a4094c2c9
Richard Kubíček <rici.kubicek@gmail.com>
2019-10-06 17:01:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 mkdir ./log
 chmod a+x low-level-spi.py
-sudo apt-get update
-sudo apt-get install python-pip
+sudo apt-get -y --force-yes  update
+sudo apt-get -y --force-yes install python-pip
 pip install -r requirements.txt
-sudo apt-get install mosquitto
-sudo apt-get install mosquitto-clients
+sudo apt-get -y --force-yes install mosquitto
+sudo apt-get -y --force-yes install mosquitto-clients
