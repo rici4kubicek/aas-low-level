82abd75d90b0ae7b471bb32639a954075b0f0e1a
Richard Kubíček <rici.kubicek@gmail.com>
2019-10-29 20:34:54 +0100

    APA102: check input range of colours

--- tools/apa102.py
+++ tools/apa102.py
@@ -25,6 +25,9 @@ class APA102(object):
     def prepare_data(self, r, g, b, brightness, led_idx):
         _brightness = valmap(brightness, 0, 100, 0, 31)
         self._led_array[led_idx * 4 + 4] = 0xe0 + _brightness
+        r = valmap(r, 0, 255, 0, 255)
+        g = valmap(g, 0, 255, 0, 255)
+        b = valmap(b, 0, 255, 0, 255)
         self._led_array[led_idx * 4 + 5] = b
         self._led_array[led_idx * 4 + 6] = g
         self._led_array[led_idx * 4 + 7] = r
