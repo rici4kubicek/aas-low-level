25afc0b8ae98b2d9d89f0984581eadbf7275806d
Richard Kubíček <rici.kubicek@gmail.com>
2019-10-29 20:30:06 +0100

    apa102: refactoring

--- tools/apa102.py
+++ tools/apa102.py
@@ -1,28 +1,33 @@
 from tools import *
+import logging
+
+# create logger
+module_logger = logging.getLogger('apa102')
 
 
 class APA102(object):
     def __init__(self, led_count):
-        self.led_count = led_count
-        self.led_array = [0, 0, 0, 0]
+        self._logger = logging.getLogger('apa102')
+        self._led_count = led_count
+        self._led_array = [0, 0, 0, 0]
         i = 0
-        while i < led_count:
-            self.led_array.append([0])
-            self.led_array.append([0])
-            self.led_array.append([0])
-            self.led_array.append([0])
+        while i < self._led_count:
+            self._led_array.append([0])
+            self._led_array.append([0])
+            self._led_array.append([0])
+            self._led_array.append([0])
             i = i + 1
-        self.led_array.append(0xff)
-        self.led_array.append(0xff)
-        self.led_array.append(0xff)
-        self.led_array.append(0xff)
+        self._led_array.append(0xff)
+        self._led_array.append(0xff)
+        self._led_array.append(0xff)
+        self._led_array.append(0xff)
 
     def prepare_data(self, r, g, b, brightness, led_idx):
         _brightness = valmap(brightness, 0, 100, 0, 31)
-        self.led_array[led_idx * 4 + 4] = 0xe0 + _brightness
-        self.led_array[led_idx * 4 + 5] = b
-        self.led_array[led_idx * 4 + 6] = g
-        self.led_array[led_idx * 4 + 7] = r
+        self._led_array[led_idx * 4 + 4] = 0xe0 + _brightness
+        self._led_array[led_idx * 4 + 5] = b
+        self._led_array[led_idx * 4 + 6] = g
+        self._led_array[led_idx * 4 + 7] = r
 
     def get_data(self):
-        return self.led_array
+        return self._led_array
