3cf5daed057a637f775b7186bfe64e10130d8c7b
Richard Kubíček <rici.kubicek@gmail.com>
2019-11-05 13:50:33 +0100

    load-font: test script

---
+++ i2c/load_fonts.py
@@ -0,0 +1,14 @@
+
+import os
+import fnmatch
+from PIL import ImageFont
+path = './static/'
+
+files = os.listdir(path)
+pattern = "*.ttf"
+fonts = {}
+for font in files:
+    if fnmatch.fnmatch(font, pattern):
+        fonts[font[:-4] + "-" + str(10) + ""] = ImageFont.truetype("" + path + font + "", 10)
+
+print(fonts)
