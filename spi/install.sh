5eabb7a504537a31a8364b9b0a99b049fc937b8c
Richard Kubíček <rici.kubicek@gmail.com>
2020-02-16 23:17:09 +0100

    Update spi install.sh and use python3

--- spi/install.sh
+++ spi/install.sh
@@ -2,12 +2,15 @@
 mkdir ./log
 chmod a+x low-level-spi.py
 sudo apt-get -y --force-yes  update
-sudo apt-get -y --force-yes install python-pip
-pip install -r requirements.txt
+sudo apt-get -y --force-yes install python3-pip
+sudo pip3 install -r requirements.txt
 sudo apt-get -y --force-yes install mosquitto
 sudo apt-get -y --force-yes install mosquitto-clients
 cd
 git clone https://github.com/nightseas/python-nightWiring
 cd python-nightWiring
 git submodule init && git submodule update
-sudo python setup.py install
+sudo python3 setup.py install
+cd
+sudo rm -r python-nightWiring
+cd aas-low-level/spi
